c399946b89f9935148f17943577dbec98fb7587e
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2025-09-04 18:52:07 +0900

    inGame管理クラス作成

---
+++ DirectXGame/InGameController.cpp
@@ -0,0 +1,9 @@
+#include "InGameController.h"
+
+InGameController::InGameController() {}
+
+InGameController::~InGameController() {}
+
+void InGameController::Initialize() {}
+void InGameController::Update() {}
+void InGameController::Draw() {}
---
+++ DirectXGame/InGameController.h
@@ -0,0 +1,13 @@
+#pragma once
+class InGameController {
+public:
+
+	InGameController();
+	~InGameController();
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+private:
+};
