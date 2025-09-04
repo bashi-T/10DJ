8e4a33365e36580a81b9f81d0ce7de7f630c68e0
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2025-09-04 16:58:02 +0900

    シーンを越えてやり取りしたいものをSceneManagerに

--- DirectXGame/Scenes/SceneManager.h
+++ DirectXGame/Scenes/SceneManager.h
@@ -18,9 +18,15 @@ public:
 
 	void SetStageNumber(int32_t number) { stageNumber = number; }
 	int32_t GetStageNumber() { return stageNumber; }
+	void SetIsClear(bool isClear) { isClear_ = isClear; }
+	bool GetIsClear() { return isClear_; }
+	void SetNumThrow(int num) { numThrow = num; }
+	int32_t GetNumRhrow() { return numThrow; }
 
 protected:
 	int32_t stageNumber = 0;
+	bool isClear_ = false;
+	int32_t numThrow = 0;
 
 private:
 	std::unique_ptr<BaseScene> currentScene;
