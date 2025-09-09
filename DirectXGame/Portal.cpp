456596e084aa1698e09bbd24ee722835f133b8d1
k022g0055 <k022g0055@g.neec.ac.jp>
2025-09-10 02:24:57 +0900

    ポータルクラス作成

---
+++ DirectXGame/Portal.cpp
@@ -0,0 +1,21 @@
+#include "Portal.h"
+
+Portal::Portal()
+{
+}
+
+Portal::~Portal()
+{
+}
+
+void Portal::Initialize()
+{
+}
+
+void Portal::Update()
+{
+}
+
+void Portal::Draw()
+{
+}
---
+++ DirectXGame/Portal.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <KamataEngine.h>
+class Portal
+{
+public:
+	Portal();
+	~Portal();
+	void Initialize();
+	void Update();
+	void Draw();
+	KamataEngine::Vector2 GetPosition() const { return position_; }
+	KamataEngine::Vector2 GetSize() const { return size_; }
+private:
+};
+
