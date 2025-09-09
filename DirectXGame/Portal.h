01bdcd9336f78e780ff42fadaae3d884b81ea922
k022g0055 <k022g0055@g.neec.ac.jp>
2025-09-10 03:20:21 +0900

    紙のリソース追加

--- DirectXGame/Portal.h
+++ DirectXGame/Portal.h
@@ -8,8 +8,13 @@ public:
 	void Initialize();
 	void Update();
 	void Draw();
-	//KamataEngine::Vector2 GetPosition() const { return position_; }
-	//KamataEngine::Vector2 GetSize() const { return size_; }
+	KamataEngine::Vector2 GetPosition() const { return position_; }
+	KamataEngine::Vector2 GetSize() const { return size_; }
 private:
+	KamataEngine::Sprite* portal_ = nullptr;
+	KamataEngine::Vector2 position_{};
+	KamataEngine::Vector2 size_{};
+	uint32_t textureHandle_ = 0;
+	float portalSize_ = 100.0f;
 };
 
--- DirectXGame/TextureLoader.cpp
+++ DirectXGame/TextureLoader.cpp
@@ -5,7 +5,7 @@ using namespace KamataEngine;
 TextureLoader::TextureLoader()
 {
 	playerTextureHandle_ = TextureManager::Load("hand_1.png");
-	wadPaperTextureHandle_ = TextureManager::Load("sample.png");
+	wadPaperTextureHandle_ = TextureManager::Load("wadPaper.png");
 	trashCanTextureHandle_ = TextureManager::Load("tex1.png");
 	brockTextureHandle_ = TextureManager::Load("sample.png");
 }
