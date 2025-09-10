86ecd45f8052582b047505afaac7169e6a261f80
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2025-09-10 10:18:07 +0900

    ittan

--- DirectXGame/Math/Collision.cpp
+++ DirectXGame/Math/Collision.cpp
@@ -126,3 +126,11 @@ bool isCollision(const Sphere& sphere1, const Sphere& sphere2)
 		return false;
 	}
 }
+
+void PushBack(KamataEngine::Vector2& garbagePos, KamataEngine::Vector2& garbageVector, KamataEngine::Vector2& garbageSize, KamataEngine::Vector2& wallPos, KamataEngine::Vector2& wallSize)
+{
+	if (garbagePos.x - wallPos.x > || garbagePos.x < wallPos.x)
+	{
+		garbageVector.x *= -1;
+	}
+}
